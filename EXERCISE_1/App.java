02d373858bb8626c9ab1eec051b53fead500b9d7
Mariel A. Monte de Ramos <111344331+mariel-ramos@users.noreply.github.com>
2022-09-08 01:46:57 +0800

    Create App.java

---
+++ EXERCISE_1/App.java
@@ -0,0 +1,73 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+
+        // Enemy Class
+        Enemy boss = new Enemy();
+
+        boss.damage = 50;
+        boss.health = 500;
+        boss.armor = 100;
+        boss.hasMagicImmunity = true;
+        boss.name = "Roshan";
+        boss.enemyDialogue();
+
+        Enemy skeleton = new Enemy();
+
+        skeleton.damage = 5;
+        skeleton.health = 10;
+        skeleton.armor = 5;
+        skeleton.hasMagicImmunity = false;
+        skeleton.name = "Common Skeleton Warrior";
+        skeleton.enemyDialogue();
+
+        Enemy dragon = new Enemy();
+
+        dragon.damage = 40;
+        dragon.health = 400;
+        dragon.armor = 75;
+        dragon.hasMagicImmunity = false;
+        dragon.name = "Ancient Wyvern";
+        dragon.enemyDialogue();
+
+        // Exercise 1
+        // Weapon Class
+        Weapon Sword = new weapon();
+
+       Sword.Name = "Lapu-Lapu";
+       Sword.Weapontype = "Lumad Sword";
+       Sword.damage = 80;
+       Sword.weight = 30;
+       Sword.Rarity = Legendary;
+       Sword.description = " Lapu lapus only weapon to kill powerful enemies ";
+       Sword.weaponDecription();
+
+        Weapon Sword = new Weapon("Lapu-Lapu", "Lumad Sword", 80, 30, "Lapu lapus only weapon to kill powerful enemies");
+            Sword.weaponDecription();
+        
+    }
+}

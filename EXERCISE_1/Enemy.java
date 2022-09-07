6fe2c4fba94e2ab1aabb43b886a27fb6f2450d8a
Mariel A. Monte de Ramos <111344331+mariel-ramos@users.noreply.github.com>
2022-09-08 01:48:58 +0800

    Create Enemy.java

---
+++ EXERCISE_1/Enemy.java
@@ -0,0 +1,15 @@
+public class Enemy {
+    // Enemy attributes
+    int damage;
+    int health;
+    int armor;
+    boolean hasMagicImmunity;
+    String name;
+    String greeting;
+
+    // Enemy method
+    public void enemyDialogue() {
+        System.out.println("\nFear me player for my name is " + name + " and I deal " + damage + " damage!");
+        System.out.println (" die " + greetings);
+    }
+}

c80c5c7a898ae1c252a5ad9fd5374969bbc64c7b
Mariel A. Monte de Ramos <111344331+mariel-ramos@users.noreply.github.com>
2022-09-08 01:48:09 +0800

    Create Character.java

---
+++ EXERCISE_1/Character.java
@@ -0,0 +1,15 @@
+public class Character {
+    // Character attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+    String greeting;
+
+    // Character method
+    public void sayMyName() {
+        System.out.println("\nThe names " + name);
+        System.out.println("Greeting: " +greetings;
+        //System.out.println("\nStrength: " + strength + "\nAgility: " + agility + "\nIntelligence: " + intelligence);
+    }
+}
